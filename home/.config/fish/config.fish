db23482fa957baddb9b42424e0b49392e9e9ac8d
orostachys <mczoden@orostachys.com>
2015-09-22 11:04:58 +0800

    fish: PATH

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,5 +5,5 @@ set fish_greeting
 
 env -i HOME=$HOME dash -l -c printenv | sed -e '/PATH/s/:/ /g;s/=/ /;s/^/set -x /' | source
 
-set --universal fish_user_paths ~/pro/le-jour $fish_user_paths
+set PATH ~/pro/le-jour $PATH
 set --unexport fish_color_valid_path
