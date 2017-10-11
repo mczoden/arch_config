5e3d0b79c26f9b315a788901cc54a42a298f0829
Summer Sun <summersun@md101.local>
2017-10-11 21:57:21 +0800

    git branch in fish prompt

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -14,7 +14,18 @@ function fish_prompt --description "Write out the prompt"
         set -g __fish_prompt_hostname (hostname | cut -d . -f 1)
     end
 
+    if not set -q __git_cb
+        set __branch (git branch ^/dev/null | grep \* | sed 's/* //')
+        if not test -z $__branch
+            if test $__branch = "master"
+                set __git_cb "["(set_color green)$__branch(set_color normal)"]"
+            else
+                set __git_cb "["(set_color brown)$__branch(set_color normal)"]"
+            end
+        end
+    end
+
     echo -s "$prefix" [ (set_color $fish_color_cwd) "$USER" @ "$__fish_prompt_hostname" (set_color normal) ] \
-        [ (set_color blue --bold) (echo $PWD | sed -e "s|$realhome|~|") (set_color normal) ]
+        [ (set_color blue --bold) (echo $PWD | sed -e "s|$realhome|~|") (set_color normal) ] $__git_cb
     echo -n -s "$suffix"
 end
